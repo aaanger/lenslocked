277eb7f67d439842b027cf6a8379a514c3b377f8
aaanger <166844879+aaanger@users.noreply.github.com>
2024-08-24 16:40:21 +0300

    Add files via upload

--- views/template.go
+++ views/template.go
@@ -1,1 +1,40 @@
+package views
 
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	htmlTpl *template.Template
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.htmlTpl.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "Error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
+	htmlTpl, err := template.ParseFS(fs, pattern...)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %v", err)
+	}
+	return Template{
+		htmlTpl: htmlTpl,
+	}, nil
+}
+
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
