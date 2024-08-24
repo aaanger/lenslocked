96aeae281f84cc3f48b75c84dc5bce35fdcca78b
aaanger <166844879+aaanger@users.noreply.github.com>
2024-08-24 16:42:05 +0300

    Add files via upload

--- controllers/static.go
+++ controllers/static.go
@@ -1,1 +1,40 @@
+package controllers
 
+import (
+	"html/template"
+	"net/http"
+)
+
+type Static struct {
+	Template Template
+}
+
+func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	static.Template.Execute(w, nil)
+}
+
+func StaticHandler(tpl Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{Question: "Is there a free version?",
+			Answer: "Yes",
+		},
+		{Question: "What are your support hours?",
+			Answer: "We have support staff answering emails 24/7",
+		},
+		{Question: "How do I contact support?",
+			Answer: `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
+	}
+}
